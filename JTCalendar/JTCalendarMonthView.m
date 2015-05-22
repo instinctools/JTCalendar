68b4484775c38e307ee99f0fd11a96a63e99a18f
Taras Chernyshenko <taras.chernyshenko@lezgro.com>
2015-05-22 14:53:53 +0300

    automatic calculation of weeks for the month

--- JTCalendar/JTCalendarMonthView.m
+++ JTCalendar/JTCalendarMonthView.m
@@ -19,7 +19,7 @@
 	NSUInteger currentMonthIndex;
 	BOOL cacheLastWeekMode; // Avoid some operations
 };
-
+@property (nonatomic, strong) NSDate *firstDayDate;
 @end
 
 @implementation JTCalendarMonthView
@@ -46,7 +46,34 @@
 	return self;
 }
 
+- (void)setFirstDayDate:(NSDate *)firstDayDate {
+	_firstDayDate = firstDayDate;
+//	[self commonInit];
+	[self reloadData];
+}
+
+- (NSUInteger)weeksCount {
+	NSDate *date = self.firstDayDate;
+	if (date == nil) {
+		return 0;
+	}
+	NSCalendar *calender = self.calendarManager.calendarAppearance.calendar;
+	NSRange days = [calender rangeOfUnit:NSDayCalendarUnit
+	                              inUnit:NSMonthCalendarUnit
+	                             forDate:date];
+	NSDateComponents *comps = [calender components:NSYearCalendarUnit | NSMonthCalendarUnit | NSWeekCalendarUnit | NSWeekdayCalendarUnit fromDate:date];
+    //Not sure why, but setDay: for 31 create 30th day, so I just make this hack
+	[comps setDay:days.length + 1];
+	NSDate *lastDay = [calender dateFromComponents:comps];
+	NSRange weekRange = [calender rangeOfUnit:NSWeekCalendarUnit inUnit:NSMonthCalendarUnit forDate:lastDay];
+	NSInteger weeksCount = weekRange.length;
+	return weeksCount;
+}
+
 - (void)commonInit {
+	for (UIView *subview in self.subviews) {
+		[subview removeFromSuperview];
+	}
 	NSMutableArray *views = [NSMutableArray new];
 
 	{
@@ -79,7 +106,7 @@
 		weeksToDisplay = 2.;
 	}
 	else {
-		weeksToDisplay = (CGFloat)(WEEKS_TO_DISPLAY + 1); // + 1 for weekDays
+		weeksToDisplay = (CGFloat)([weeksViews count] + 1); // + 1 for weekDays
 	}
 
 	CGFloat y = 0;
@@ -100,6 +127,7 @@
 
 - (void)setBeginningOfMonth:(NSDate *)date {
 	NSDate *currentDate = date;
+	self.firstDayDate = date;
 
 	NSCalendar *calendar = self.calendarManager.calendarAppearance.calendar;
 
@@ -114,19 +142,23 @@
 		}
 	}
 
-	for (JTCalendarWeekView *view in weeksViews) {
-		view.currentMonthIndex = currentMonthIndex;
-		[view setBeginningOfWeek:currentDate];
-
-		NSDateComponents *dayComponent = [NSDateComponents new];
-		dayComponent.day = 7;
-
-		currentDate = [calendar dateByAddingComponents:dayComponent toDate:currentDate options:0];
-
-		// Doesn't need to do other weeks
-		if (self.calendarManager.calendarAppearance.isWeekMode) {
-			break;
-		}
+	for (NSUInteger i = 0; i < [weeksViews count]; i++) {
+        JTCalendarWeekView *view = weeksViews[i];
+        //hide if this week is not in current month
+		[view setHidden:i >= [self weeksCount]];
+        
+        view.currentMonthIndex = currentMonthIndex;
+        [view setBeginningOfWeek:currentDate];
+        
+        NSDateComponents *dayComponent = [NSDateComponents new];
+        dayComponent.day = 7;
+        
+        currentDate = [calendar dateByAddingComponents:dayComponent toDate:currentDate options:0];
+        
+        // Doesn't need to do other weeks
+        if (self.calendarManager.calendarAppearance.isWeekMode) {
+            break;
+        }
 	}
 }
 
@@ -142,21 +174,21 @@
 }
 
 - (void)reloadData {
-    for (JTCalendarWeekView *view in weeksViews) {
-        [view reloadData];
-        if (self.calendarManager.selectedDate != nil) {
-            unsigned int flags = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit;
-            NSCalendar* calendar = [NSCalendar currentCalendar];
-            NSDateComponents* components = [calendar components:flags fromDate:self.calendarManager.selectedDate];
-            NSDate* dateOnly = [calendar dateFromComponents:components];
-            view.isSelected = [view.dates containsObject:dateOnly];
-        }
+	for (JTCalendarWeekView *view in weeksViews) {
+		[view reloadData];
+		if (self.calendarManager.selectedDate != nil) {
+			unsigned int flags = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit;
+			NSCalendar *calendar = [NSCalendar currentCalendar];
+			NSDateComponents *components = [calendar components:flags fromDate:self.calendarManager.selectedDate];
+			NSDate *dateOnly = [calendar dateFromComponents:components];
+			view.isSelected = [view.dates containsObject:dateOnly];
+		}
 
-        // Doesn't need to do other weeks
-        if (self.calendarManager.calendarAppearance.isWeekMode) {
-            break;
-        }
-    }
+		// Doesn't need to do other weeks
+		if (self.calendarManager.calendarAppearance.isWeekMode) {
+			break;
+		}
+	}
 }
 
 - (void)reloadAppearance {
@@ -177,9 +209,8 @@
 	[self.delegate calendarMonthView:self didBeginTouchCalendarMonthView:calendarWeekView withCalendarDayView:calendarDayView];
 }
 
--(void)calendarWeekView:(JTCalendarWeekView *)calendarWeekView didEndTouchCalendarDayView:(JTCalendarDayView *)calendarDayView
-{
-    [self.delegate calendarMonthView:self didEndTouchCalendarMonthView:calendarWeekView withCalendarDayView:calendarDayView];
+- (void)calendarWeekView:(JTCalendarWeekView *)calendarWeekView didEndTouchCalendarDayView:(JTCalendarDayView *)calendarDayView {
+	[self.delegate calendarMonthView:self didEndTouchCalendarMonthView:calendarWeekView withCalendarDayView:calendarDayView];
 }
 
 @end
